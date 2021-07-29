a450a895c38ada63b9edde412915d8e4d54d4458
Kucharewicz <kucharewicz.iwo@gmail.com>
2021-07-29 23:52:57 +0200

    update

---
+++ target/classes/polish_signs_script.sql
@@ -0,0 +1,4 @@
+ALTER table daily_task CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER table golden_thought CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER table user CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER table user_task CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
